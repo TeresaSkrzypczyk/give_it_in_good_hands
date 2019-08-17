f6e86edd25ff08c84057cdafe3beed60493e998c
Teresa Skrzypczyk <tereska.skrzypczyk@gmail.com>
2019-08-17 21:44:47 +0200

    header without images

--- src/components/header.js
+++ src/components/header.js
@@ -6,26 +6,55 @@ import Registration from "./registration"
 export default class Header extends Component {
     render() {
         return [
-            <div className="main-image">
-            </div>,
+            <div className="wrapper">
+                <div className="row">
 
-            <div>
-                <div key="registration">
-                    <Registration />
-                </div>,
+                    <div className="col-5">
+                        <div className="image-header" style={{border: "2px solid yellow"}}>
+                            <img src="../asstes/Home-Hero-Image.jpg" />
+                        </div>
+                    </div>
 
-                <div className="wrapper">
-                    <ul className="navigation" key="navigation">
-                        <li>Start</li>
-                        <li><Link to="simple_steps"> O co chodzi?</Link></li>
-                        <li><Link to="about_us">O nas</Link></li>
-                        <li><Link to="who_we_help">Fundacja i organizacje</Link></li>
-                        <li><Link to="contact">Kontakt</Link></li>
-                    </ul>
+                    <div className="col-7">
+
+
+                            <div className="col-12 header">
+                                <div className="registration" key="registration"><Registration />
+                                </div>
+                            </div>
+
+                            <div className="col-12 header">
+                                <ul className="navigation" key="navigation">
+                                    <li>Start</li>
+                                    <li><Link to="simple_steps"> O co chodzi?</Link></li>
+                                    <li><Link to="about_us">O nas</Link></li>
+                                    <li><Link to="who_we_help">Fundacja i organizacje</Link></li>
+                                    <li><Link to="contact">Kontakt</Link></li>
+                                </ul>
+                            </div>
+
+
+
+                            <div className="col-12">
+                                <p className="text-header">Zacznij pomagać!<br />Oddaj niechciane rzeczy w zaufane ręce</p>
+                            </div>
+
+                            <div className="col-12 image-decoration" style={{border: "2px solid yellow"}}>
+                                <img src="../asstes/Decoration.svg" />
+                            </div>
+
+                            <div className="col-12">
+                                <ul className="list-header">
+                                    <li>ODDAJ<br />RZECZY</li>
+                                    <li>ZORGANIZUJ<br />ZBIÓRKĘ</li>
+                                </ul>
+                            </div>
+
+                    </div>
                 </div>
             </div>,
 
-            <div key="three columns">
+            <div key="three columns" className="wrapper three">
                 <ThreeColumns />
             </div>
 
--- src/components/three_columns.js
+++ src/components/three_columns.js
@@ -3,12 +3,22 @@ import React, {Component} from "react";
 class ThreeColumns extends Component {
     render() {
         return (
-            <div style={{
-                weight: '100px',
-                height: '400px',
-                backgroundColor: 'brown'
-            }}>
-                <p>Three columns - część headera</p>
+            <div className="row">
+                <div className="col-4 three-columns">
+                    <h1>10</h1>
+                    <h3>ODDANYCH WORKÓW</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc<br />Pellentesque velenim a elit viverra elementuma.<br />Aliquam erat volutpat.</p>
+                </div>
+                <div className="col-4 three-columns">
+                    <h1>5</h1>
+                    <h3>WSPARTYCH ORGANIZACJI</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc<br />Pellentesque velenim a elit viverra elementuma.<br />Aliquam erat volutpat.</p>
+                </div>
+                <div className="col-4 three-columns">
+                    <h1>7</h1>
+                    <h3>ZORGANIZOWANYCH ZBIÓREK</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc<br />Pellentesque velenim a elit viverra elementuma.<br />Aliquam erat volutpat.</p>
+                </div>
             </div>
         );
     }
