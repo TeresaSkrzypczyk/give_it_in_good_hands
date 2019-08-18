1372a501c157f483194986bf342ba7cbb4db3a64
Teresa Skrzypczyk <tereska.skrzypczyk@gmail.com>
2019-08-18 22:14:32 +0200

    layaout version I

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,15 +4,32 @@ import Form from "./form";
 class Footer extends Component {
     render() {
         return [
-            <div id="contact" style={{
-                weight: '100px',
-                height: '400px',
-                backgroundColor: 'yellow'
-            }}>
-                <div key="form">
-                    <Form />
+
+            <div className="wrapper">
+                <div className="row">
+
+                    <div id="contact" className="col-7" style={{border: "1px solid pink"}}>
+                    </div>
+
+                    <div className="col-5">
+                        <div  className="col-12">
+                            <p className="text-steps">Skontaktuj się z nami</p>
+                        </div>
+
+                        <div className="col-12 image-decoration" style={{border: "2px solid yellow"}}>
+                            <img src="../asstes/Decoration.svg" alt="decoration"/>
+                        </div>
+
+                        <div>
+                            <Form />
+                        </div>
+                    </div>
+
+                    <div className="col-12">
+                        <p className="copyright">Copyright by Coders Lab</p>
+                    </div>
+
                 </div>
-                <p>TEST</p>
             </div>
             ]
     }
--- src/components/simple_steps.js
+++ src/components/simple_steps.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
 
 class SimpleSteps extends Component {
     render() {
@@ -14,24 +15,31 @@ class SimpleSteps extends Component {
                         <img src="../asstes/Decoration.svg" />
                     </div>
 
-                    <div className="col-12 gris" style={{backgroundColor: "gris"}}>
-                        <div className="col-3" style={{border: "1px solid pink"}}>
+                    <div className="col-12 grey">
+                        <div className="col-3">
                             <p>Wybierz rzeczy</p><hr />
                             <p className="special-small">urania, zabawki<br />sprzęt i inne</p>
                         </div>
-                        <div className="col-3" style={{border: "1px solid pink"}}>
+                        <div className="col-3">
                             <p>Spakuj je</p><hr />
                             <p className="special-small">skorzystaj z<br />worków na śmieci</p>
                         </div>
-                        <div className="col-3" style={{border: "1px solid pink"}}>
+                        <div className="col-3">
                             <p>Zdecyduj komu<br />chcesz pomów</p><hr />
                             <p className="special-small">wybierz zaufane<br />miejsce</p>
                         </div>
-                        <div className="col-3" style={{border: "1px solid pink"}}>
+                        <div className="col-3">
                             <p>Zamów kuriera</p><hr />
                             <p className="special-small">kurier przyjedzie<br />w dogodnym terminie</p>
                         </div>
                     </div>
+
+                    <div className="col-12">
+                        <ul className="list-header">
+                            <li key="return"><Link to="/log">ODDAJ<br />RZECZY</Link></li>
+                        </ul>
+                    </div>
+
                 </div>
             </div>
 
